c0f164b3f7c08569fce9bf2cb037c8d3152d1331
Jason Lopez <jason.lopez@Jason-Lopez-MacBook-Pro.local>
2019-11-07 16:43:32 -0600

    Material ui appbar and drawer added.

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,41 +1,97 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 
-import axios from 'axios'
-import { saveAs } from 'file-saver'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+
+import Drawer from '@material-ui/core/Drawer'
+import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import { ListIcon } from '../../icons/icon'
 
 import Loader from '../loader'
-import Menu from '../menu'
 
-import styles from './home.css'
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: 10
+  },
+  title: {
+    flexGrow: 1
+  },
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: 'auto'
+  }
+})
 
-class Home extends PureComponent {
-  constructor (props) {
-    super(props)
-    this.state = {
-      message: 'Descargando',
-      loading: false
+function Home() {
+  const classes = useStyles()
+  const [state, setState] = React.useState({
+    loading: false,
+    drawerOpen: false
+  })
+
+  const toggleDrawer = (open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return
     }
+    setState({ ...state, drawerOpen: open })
   }
 
-  downloadFile () {
-    this.setState({ loading: true })
-    axios.get('https://jlcsolutionscr.com/puntoventa/PuntoventaWCF.svc/descargaractualizacion',
-    {responseType: 'blob'})
-      .then((response) => {
-        const blob = new Blob([response.data], {type: "application/octet-stream"})
-        saveAs(blob, 'puntoventaJLC.msi')
-        this.setState({ loading: false })
-      });
-  }
-  
-  render () {
-    return (
-      <div id='id_app_content' className="home">
-        {this.state.loading && <Loader loaderText={this.state.message} isLoaderActive={this.state.loading}/>}
-        <Menu onClick={this.downloadFile.bind(this)}/>
-      </div>
-    )
-  }
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div id='id_app_content' className={classes.root}>
+      {state.loading && <Loader loaderText={this.state.message} isLoaderActive={state.loading}/>}
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+            <ListIcon style={{size: 10}}/>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Página de inicio
+          </Typography>
+          <Button color="inherit">Cerrar sessión</Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={state.drawerOpen} onClose={toggleDrawer(false)}>
+        {sideList('left')}
+      </Drawer>
+    </div>
+  )
 }
 
 export default Home
