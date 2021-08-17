61f0c7f46943e578110f79402fbc49b26e22838c
Jason Lopez <jason.lopez@affirm.com>
2021-08-16 19:17:55 -0600

    Fix issue on invoice generation

--- src/utils/domainHelper.js
+++ src/utils/domainHelper.js
@@ -402,7 +402,7 @@ export async function saveInvoiceEntity(
       IdTipoMoneda: 1,
       TipoDeCambioDolar: 0,
       IdCliente: customer.IdCliente,
-      NombreCliente: customer.NombreCliente,
+      NombreCliente: customer.NombreComercial || customer.Nombre,
       IdCondicionVenta: 1,
       PlazoCredito: 0,
       Fecha: {DateTime: timeString},
