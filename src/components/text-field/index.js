6d90e57ca9058ab971caedf78884aef4857c0569
Jason Lopez <jason.lopez@affirm.com>
2021-09-08 15:36:28 -0600

    Fix numeric format to include period

--- src/components/text-field/index.js
+++ src/components/text-field/index.js
@@ -5,7 +5,7 @@ function CustomTextField(props) {
   const { onChange, numericFormat, variant, ...restProps } = props
   const handleChange = event => {
     let value = event.target.value
-    if (numericFormat) value = event.target.value.replace(/[^0-9]/g, '')
+    if (numericFormat) value = event.target.value.replace(/[^0-9.]/g, '')
     event.target.value = value
     onChange(event)
   }
